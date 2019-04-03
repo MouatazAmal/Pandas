40ed248157fc7da70a1d3607dd03472b0f34e65b
Mouataz <mouataz.amal@gmail.com>
2019-04-03 09:26:43 +0200

    Nouvelle structure

--- src/main/java/Project/Devops/Sec/CsvParser.java
+++ src/main/java/Project/Devops/Sec/CsvParser.java
@@ -2,6 +2,7 @@ package Project.Devops.Sec;
 
 import com.opencsv.CSVReader;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -16,7 +17,6 @@ public class CsvParser {
     private final String fileName;
     private File file;
     private FileReader fileReader;
-    private CSVReader csvReader;
     private ArrayList<String[]> rows;
     private String[] columns;
     private int nbColumns ;
@@ -32,8 +32,8 @@ public class CsvParser {
             this.file = new File(this.fileName);
             if (this.file.canRead()){
                 this.fileReader = new FileReader(file);
-                this.csvReader = new CSVReader(this.fileReader);
-                this.rows = (ArrayList<String[]>) this.csvReader.readAll();
+                this.rows = new ArrayList<String[]>();
+                this.parseCsv();
                 this.columns = this.rows.get(0);
                 this.nbColumns = this.columns.length;
                 this.nbLines = this.rows.size();
@@ -42,6 +42,19 @@ public class CsvParser {
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    private void parseCsv() throws IOException {
+        BufferedReader buff = null;
+        String line = "";
+
+        buff = new BufferedReader(fileReader);
+        while ((line = buff.readLine()) != null) {
+
+            String[] row = line.split(",");
+            this.rows.add(row);
         }
 
     }
@@ -58,10 +71,6 @@ public class CsvParser {
         return fileReader;
     }
 
-    public CSVReader getCsvReader() {
-        return csvReader;
-    }
-
     public ArrayList<String[]> getRows() {
         return rows;
     }
@@ -76,6 +85,16 @@ public class CsvParser {
 
     public int getNbLines() {
         return nbLines;
+    }
+
+    public void printDataFrame(){
+        for (String[] row : this.rows){
+            for (int i =0; i<row.length;i++) {
+                System.out.print(row[i]);
+            }
+            System.out.println();
+
+        }
     }
 
 
